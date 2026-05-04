07df248b5226eed8c0b5d354946dc07449528cab
guyshonshon <gshonshon@gmail.com>
2026-05-04 15:40:53 +0300

    Also normalized unnecessary

--- question-5/main.py
+++ question-5/main.py
@@ -1,23 +1,15 @@
-def get_normalized_file_name(file_name):
-    """
-    Return the normalized file-path for a given file-name
-    """
-    return f'./{file_name}'
-
-
 def GetWordsFromFile(file_name):
     """
     Read file lines, then iterate through each line, split line into list of words
     and then add each word into a set, which takes care of duplications natively.
     """
-    normalized_file_name = get_normalized_file_name(file_name)
     try:
-        with open(normalized_file_name, 'r') as file:
+        with open(file_name, 'r') as file:
             print({word for line in file.readlines() for word in line.split()})
     except FileNotFoundError:
-        print(f'{normalized_file_name} does not exist')
+        print(f'{file_name} does not exist')
     except PermissionError:
-        print(f'Permission denied for {normalized_file_name}')
+        print(f'Permission denied for {file_name}')
 
 
 if __name__ == '__main__':
