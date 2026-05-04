c35384843d6e7b3afe9923ab1e20c8c9b2ae703e
guyshonshon <gshonshon@gmail.com>
2026-05-04 15:34:28 +0300

    Fixed various bugs, improved code readability and added can_make_it_top_ten check

--- question-1/main.py
+++ question-1/main.py
@@ -6,6 +6,9 @@ from pathlib import Path
 TREASURE_PATH = Path("treasure.txt")
 HIGHSCORES_PATH = Path("highscore.txt")  # least moves each line, sorted
 
+FORWARD_STEP = 1
+BACKWARD_STEP = 2
+
 
 # Utility function for redundancy (DRY)
 def rand_digits(digit):
@@ -21,10 +24,10 @@ def get_user_steps(index, end_of_file_index):
     """
     _input = input("How many characters? ")
 
-    assert (
-        _input.isdigit()
-    ), f"Error, you must type a number! Current index: {index} out of {end_of_file_index}"
-
+    if not _input.isdigit():
+        print(f"Error, you must type a number! Current index: {index} out of {end_of_file_index}")
+        return get_user_steps(index, end_of_file_index)
+    
     return int(_input)
 
 
@@ -49,11 +52,27 @@ def best_score():
     return (get_latest_scores() or ["N/A"])[0]
 
 
+def can_make_it_top_ten(new_score):
+    """
+    A simple validation check that ensures only scores lower than
+    the last one (always sorted on write, so order's guaranteed),
+    or if there are less than 10 scores - auto add.
+    """
+    scores = get_latest_scores()
+    if len(scores) < 10:
+        return True
+    
+    return new_score < scores[9]
+
+
 def append_score(new_score):
     """
     Fetches latest scores, add {new_score} to the list and sort,
     then, slice the first 10 entries and write them to the {HIGHSCORES_FILE}
     and return the new list.
+
+    XXX: Theoretically, question stated that we should log player's name, but we never
+    collect it, so we'll assume hodi won't notice it!
     """
     latest_scores = get_latest_scores()
     latest_scores.append(new_score)
@@ -102,61 +121,78 @@ def get_eof_index(file):
     return eof
 
 
-# Start
-print("Hidding the treasure....")
+def start_game():
+    print(f"Welcome to the Treasure game! Your best score is {best_score()} moves.\n")
+    print("")
 
 
-with open(
-    TREASURE_PATH, "w"
-) as file:  # w overwrites content so no need to delete pre-existing
-    for digit in range(0, 10):
-        file.write(rand_digits(digit))
+    with open(
+        TREASURE_PATH, "w"
+    ) as file:  # w overwrites content so no need to delete pre-existing
+        for digit in range(0, 10):
+            file.write(rand_digits(digit))
 
-    file.write("TREASURE")  # appends the treasure word
+        file.write("TREASURE")  # appends the treasure word
 
-    for digit in range(9, -1, -1):  # reverse indexing loop
-        file.write(rand_digits(digit))
+        for digit in range(9, -1, -1):  # reverse indexing loop
+            file.write(rand_digits(digit))
 
 
-BACKWARD_STEP = 1
-FORWARD_STEP = 2
-# STEP 2:
-with open(TREASURE_PATH, "r") as file:
-    index = 0
-    user_steps = 0
-    moves = 0
-    max_index = get_eof_index(file)
 
-    print(
-        f"Your best score is {best_score()} moves\nThe treasure may be found within {max_index} steps"
-    )
-    while True:
-        direction = int(input("Where do you want to move? [1- forward 2-backwards]: "))
-        moves += 1
-        user_steps = get_user_steps(index, max_index)
+    # STEP 2:
+    with open(TREASURE_PATH, "r") as file:
+        index = 0
+        user_steps = 0
+        moves = 0
+        max_index = get_eof_index(file)
 
-        if direction == BACKWARD_STEP:
-            if index + user_steps > max_index:
-                print(f"You went too far! You can go up to {max_index - index} steps")
-                continue  # let user enter input again
-            index += user_steps
-
-        elif direction == FORWARD_STEP:
-            if index - user_steps < 0:
-                print(f"Too many steps, you can travel back up to {index} steps")
+        print(
+            f"Hidding the treasure.... it may be found within {max_index} steps"
+        )
+        while True:
+            try:
+                direction = int(input("Where do you want to move? [1- forward 2-backwards]: "))
+            except ValueError:
+                print("You may only use 1-forward or 2-backwards as a direction.")
                 continue
-            index -= user_steps
 
-        file.seek(index)
-        index_char = file.read(1)
+            moves += 1
+            user_steps = get_user_steps(index, max_index)
 
-        if index_char in "TREASURE":
-            print("#" * 50)
-            print(f"You found the treasure '{index_char}' in {moves} attempts.")
-            print("#" * 50)
-            append_score(moves)
-            break
-        else:
-            print(f"Not here... that was {index_char}! Try again! ({moves=}, {index=})")
+            if direction == FORWARD_STEP:
+                if index + user_steps >= max_index:
+                    print(f"You went too far! You can go up to {max_index - index - 1} steps")
+                    continue
 
-        print("-" * 50)  # separator for ease of reading
+                index += user_steps
+
+            elif direction == BACKWARD_STEP:
+                if index - user_steps < 0:
+                    print(f"Too many steps, you can travel back up to {index} steps")
+                    continue
+
+                index -= user_steps
+            else:
+                print("You may only use 1-forward or 2-backwards as a direction.")
+                continue
+            
+            file.seek(index)
+            index_char = file.read(1)
+
+            if index_char in "TREASURE":
+                print("#" * 50)
+                print(f"You found the treasure '{index_char}' in {moves} attempts.")
+                print("#" * 50)
+                if can_make_it_top_ten(moves):
+                    append_score(moves)
+                else:
+                    print("Awwww!! You didn't make it into top 10 least moves")
+                break
+            else:
+                print(f"Not here... that was {index_char}! Try again! ({moves=}, {index=})")
+
+            print("-" * 50)  # separator for ease of reading
+
+
+if __name__ == "__main__":
+    start_game()
