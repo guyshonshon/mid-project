29fe19426e53f2d58ccefb5d855416a8c8908779
guyshonshon <gshonshon@gmail.com>
2026-05-04 15:49:48 +0300

    Also normalized unnecessary

--- question-7/main.py
+++ question-7/main.py
@@ -1,25 +1,15 @@
-def get_normalized_file_name(file_name):
-    """
-    Return the normalized file-path for a given file-name
-    """
-    return f'./{file_name}'
-
-
 def WriteReverse(input_file, output_file):
     """
     Open input and output file with corresponding modes,
     Then read input's content, reverse it and write to output.
     """
-    normalized_input_name = get_normalized_file_name(input_file)
-    normalized_output_name = get_normalized_file_name(output_file)
-
     try:
-        with open(normalized_input_name, 'r') as input, open(normalized_output_name, 'w') as output:
+        with open(input_file, 'r') as input, open(output_file, 'w') as output:
             output.write(input.read().strip()[::-1])
     except FileNotFoundError:
         print(f'{input_file} does not exist')
     except PermissionError:
-        print(f'Permission denied for {normalized_input_name} or {normalized_output_name}')
+        print(f'Permission denied for {input_file} or {output_file}')
 
 
 if __name__ == '__main__':
